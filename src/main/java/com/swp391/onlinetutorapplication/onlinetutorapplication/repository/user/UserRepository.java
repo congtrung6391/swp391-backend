380ab1e3bfcc35a1f7a7557af80727f9f493ead2
pvnamm10 <nampvse151536@fpt.edu.vn>
2021-09-25 13:31:31 +0700

    thêm annotation

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/repository/user/UserRepository.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/repository/user/UserRepository.java
@@ -4,11 +4,12 @@ import com.swp391.onlinetutorapplication.onlinetutorapplication.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
