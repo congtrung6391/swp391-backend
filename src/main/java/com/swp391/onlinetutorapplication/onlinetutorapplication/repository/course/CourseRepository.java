e69aaadd55dc986a83b40ee84bd7b7feac30c035
thinhdph2310 <thinhdphse151538@fpt.edu.vn>
2021-10-03 17:30:10 +0700

    update code ve may'

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/repository/course/CourseRepository.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/repository/course/CourseRepository.java
@@ -4,6 +4,21 @@ import com.swp391.onlinetutorapplication.onlinetutorapplication.model.courses.Co
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CourseRepository {
+public interface CourseRepository extends JpaRepository<Course,Long>{
+
+    Optional<Course> findByCourseDescription(String courseDes);
+    Optional<Course> findByCourseName(String courseName);
+    Optional<Course> findByCourseNameAndCourseDescription(String courseName, String courseDes);
+    Optional<Course> findBySubject(String subject);
+    Optional<Course> findByCost(double cost);
+    Optional<Course> findByCostAndSubject(double cost, String subject);
+    Optional<Course> findByIdAndCourseStatusIsTrue(Long id);
+
+    @Override
+    List<Course> findAll();
+    List<Course> findAllByCourseStatusIsTrue();
 }
