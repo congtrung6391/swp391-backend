74bed1f215c6c6c0e62cdcc783fd8105f9700cc4
thinhdph2310 <thinhdphse151538@fpt.edu.vn>
2021-10-03 23:59:52 +0700

    Update Course API

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/controller/courseController/CourseManagementController.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/controller/courseController/CourseManagementController.java
@@ -1,8 +1,13 @@
 package com.swp391.onlinetutorapplication.onlinetutorapplication.controller.courseController;
 
+import com.swp391.onlinetutorapplication.onlinetutorapplication.model.courses.Course;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.courseRequest.CourseUpdateRequest;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.authResponse.MessageResponse;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.authResponse.StatusResponse;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.courseResponse.CourseResponse;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.service.courseService.courseServiceInterface.CourseServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -18,24 +23,35 @@ public class CourseManagementController {
     private CourseServiceInterface courseService;
 
     @GetMapping("/all-courses")
-    public ResponseEntity<?> getAllCourseForStudent(){
+    public ResponseEntity<?> getAllCourseForStudent() {
         try {
             return ResponseEntity.ok().body(courseService.getAllCourseInformationForStudent());
-        }catch (NoSuchElementException ex){
+        } catch (NoSuchElementException ex) {
             return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
         }
     }
-
 
 
     @GetMapping("/admin/manage/all-courses")
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('SUPER_ADMIN')")
-    public ResponseEntity<?> getAllCourseForAdmin(){
+    public ResponseEntity<?> getAllCourseForAdmin() {
         try {
             return ResponseEntity.ok().body(courseService.getAllCourseInformationForAdmin());
-        }catch (NoSuchElementException ex){
+        } catch (NoSuchElementException ex) {
             return ResponseEntity.badRequest().body(new MessageResponse(ex.getMessage()));
         }
     }
 
+    // localhost:8080/api/course/admin/:id
+    @PutMapping("/admin/{courseId}")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('SUPER_ADMIN') or hasAuthority('TUTOR')")
+    public ResponseEntity<?> updateCourse(@RequestBody CourseUpdateRequest request, @RequestHeader(name = "Authorization") String accessToken, @PathVariable(name = "courseId") String id) {
+        Course course = courseService.updateCourse(request, Long.parseLong(id), accessToken);
+        if (course == null) {
+            return ResponseEntity.badRequest().body(new StatusResponse("Update Failed", "false"));
+        } else {
+            return ResponseEntity.ok().body(new CourseResponse(course, "true"));
+        }
+    }
 }
+
--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/userService/userServiceImplement/UserManagementImplement.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/userService/userServiceImplement/UserManagementImplement.java
@@ -1,7 +1,5 @@
 package com.swp391.onlinetutorapplication.onlinetutorapplication.service.userService.userServiceImplement;
 
-import com.swp391.onlinetutorapplication.onlinetutorapplication.model.role.ERole;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.model.role.Role;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.model.user.User;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.repository.role.RoleRepository;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.repository.user.UserRepository;
@@ -15,9 +13,6 @@ import org.springframework.stereotype.Service;
 public class UserManagementImplement implements UserManagementInterface {
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
 
     @Override
     public void deleteUser(Long id) {
