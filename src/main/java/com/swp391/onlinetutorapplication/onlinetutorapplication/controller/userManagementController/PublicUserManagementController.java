48e65f5b724b05a673347e238f27fd0fe1b2d844
pvnamm10 <nampvse151536@fpt.edu.vn>
2021-10-09 14:26:05 +0700

    chia lai user management trong package userManagement, sua lai update user

---
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/controller/userManagementController/PublicUserManagementController.java
@@ -0,0 +1,44 @@
+package com.swp391.onlinetutorapplication.onlinetutorapplication.controller.userManagementController;
+
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.userRequest.UpdateProfileRequest;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.authResponse.MessageResponse;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.responseMessage.ErrorMessageResponse;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.service.userService.userServiceInterface.UserManagementInterface;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.service.userService.userServiceInterface.UserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/public")
+public class PublicUserManagementController {
+
+    @Autowired
+    private UserServiceInterface userService;
+
+    @Autowired
+    private UserManagementInterface userManagement;
+
+    @PostMapping("/user/{id}")
+    @PreAuthorize("hasAuthority('SUPER_ADMIN') or hasAuthority('ADMIN') or hasAuthority('TUTOR') or hasAuthority('STUDENT')")
+    public ResponseEntity<?> updateUser(@RequestHeader(name = "Authorization") String accessToken,@PathVariable("id") Long id, @RequestBody UpdateProfileRequest updateProfileRequest) {
+        try {
+            userManagement.updateUser(accessToken,id, updateProfileRequest);
+            return ResponseEntity.ok().body(new MessageResponse("User id: "+id + " has been updated."));
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().body(new ErrorMessageResponse(ex.getMessage()));
+        }
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<?> responseUserProfile(@PathVariable("id") Long id){
+        try{
+            return ResponseEntity.ok().body(userManagement.getUserProfile(id));
+        }catch (Exception ex){
+            return ResponseEntity.badRequest().body(new ErrorMessageResponse(ex.getMessage()));
+        }
+    }
+}
--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/userService/userServiceImplement/UserManagementImplement.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/userService/userServiceImplement/UserManagementImplement.java
@@ -2,26 +2,18 @@ package com.swp391.onlinetutorapplication.onlinetutorapplication.service.userSer
 
 import com.swp391.onlinetutorapplication.onlinetutorapplication.model.role.Role;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.model.role.ERole;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.model.role.Role;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.model.user.User;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.model.refreshToken.RefreshToken;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.resetPasswordRequest.ResetPasswordRequest;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.userRequest.UpdateProfileRequest;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.utils.jwtUtils.JWTUtils;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.userRequest.LoginRequest;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.request.userRequest.RegistrationRequest;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.authResponse.JwtResponse;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.authResponse.MessageResponse;
+import com.swp391.onlinetutorapplication.onlinetutorapplication.payload.response.userResponse.UserProfileResponse;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.repository.role.RoleRepository;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.repository.user.UserRepository;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.model.userDetails.UserDetailsImplement;
-import com.swp391.onlinetutorapplication.onlinetutorapplication.service.mailSenderService.MailSenderService;
 import com.swp391.onlinetutorapplication.onlinetutorapplication.service.userService.userServiceInterface.UserManagementInterface;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+import javax.validation.constraints.Null;
 import java.util.*;
 
 
@@ -42,20 +34,87 @@ public class UserManagementImplement implements UserManagementInterface{
 
     @Override
     public User getUserById(Long id) {
-        return userRepository.findById(id).get();
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> {
+                    throw new NoSuchElementException("Not found user");
+                });
+        return user;
+    }
+
+    @Override
+    public UserProfileResponse getUserProfile(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(()-> {
+                    throw new NoSuchElementException("Not found user");
+                });
+        return new UserProfileResponse(
+                user.getUsername(),
+                user.getFullName(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getGrade(),
+                user.getAddress(),
+                user.getFacebookUrl(),
+                user.getAffiliate(),
+                user.getGpa(),
+                user.getGender()
+        );
     }
 
     @Override
     public void saveUser(User user) { userRepository.save(user);}
 
     @Override
-    public void updateUser(Long id, UpdateProfileRequest updateProfileRequest) {
-        User userFromDb = userRepository.findById(id).get();
-        System.out.println(userFromDb.toString());
-        userFromDb.setPhone(updateProfileRequest.getPhone());
-        userFromDb.setEmail(updateProfileRequest.getEmail());
-        userFromDb.setFullName(updateProfileRequest.getFullName());
-        userRepository.save(userFromDb);
+    public void updateUser(String accessToken, Long id, UpdateProfileRequest updateProfileRequest) {
+        accessToken = accessToken.replaceAll("Bearer ","");
+        User user = userRepository.findByAuthorizationToken(accessToken)
+                .orElseThrow(()-> {
+                    throw new NoSuchElementException("Not found user");
+                });
+        if(user.getId() != id){
+            throw new IllegalStateException("You are not allowed to change other people's accounts");
+        }
+        if(!updateProfileRequest.getEmail().equals(user.getEmail())){
+            if(updateProfileRequest.getEmail().isEmpty()){
+                throw new IllegalStateException("Email not null");
+            }
+           user.setEmail(updateProfileRequest.getEmail());
+        }
+        if(!updateProfileRequest.getPhone().equals(user.getPhone())){
+            if(updateProfileRequest.getPhone().isEmpty()){
+                user.setPhone(null);
+            }
+            user.setPhone(updateProfileRequest.getPhone());
+        }
+        if(!updateProfileRequest.getFullName().equals(user.getFullName())){
+            user.setFullName(updateProfileRequest.getFullName());
+        }
+        if(!updateProfileRequest.getGrade().equals(user.getGrade())){
+            user.setGrade(updateProfileRequest.getGrade());
+        }
+        if(!updateProfileRequest.getAddress().equals(user.getAddress())){
+            user.setAddress(updateProfileRequest.getAddress());
+        }
+
+        if(!updateProfileRequest.getAffiliate().equals(user.getAffiliate())){
+            user.setAffiliate(updateProfileRequest.getAffiliate());
+        }
+
+        if(!updateProfileRequest.getFacebookUrl().equals(user.getFacebookUrl())){
+            user.setFacebookUrl(updateProfileRequest.getFacebookUrl());
+        }
+
+        if(!updateProfileRequest.getGender().equals(user.getGender())){
+            user.setGender(updateProfileRequest.getGender());
+        }
+        if(!updateProfileRequest.getGpa().equals(user.getGpa())){
+            user.setGpa(updateProfileRequest.getGpa());
+        }
+
+
+
+        userRepository.save(user);
+
     }
 
     @Override
