50777a8f67f2743c3b253b6fd5c0c2a4e445442a
congtrung6391 <congtrung6391@gmail.com>
2021-10-12 01:53:00 +0700

    update

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/courseService/courseServiceImplement/CourseServiceImplement.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/courseService/courseServiceImplement/CourseServiceImplement.java
@@ -235,7 +235,6 @@ public class CourseServiceImplement implements CourseServiceInterface {
         if (request.isAction() == true) {
             course.setCourseStatus(false);
             courseRepository.save(course);
-
         } else {
             course.setStudent(null);
             courseRepository.save(course);
