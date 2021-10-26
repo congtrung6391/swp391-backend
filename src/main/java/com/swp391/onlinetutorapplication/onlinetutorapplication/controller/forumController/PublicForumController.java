9c1c4e978002c92daa2e842a7a1e0e8fe75bd841
Huy <giahuy120701@gmail.com>
2021-10-26 10:57:27 +0700

    Fix after merge dev

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/controller/forumController/PublicForumController.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/controller/forumController/PublicForumController.java
@@ -151,7 +151,6 @@ public class PublicForumController {
         }
     }
 
-
     @PutMapping ("/question/{questionId}/updateanswer/{answerId}")
     @PreAuthorize("hasAuthority('STUDENT') or hasAuthority('TUTOR') or hasAuthority('ADMIN') or hasAuthority('SUPER_ADMIN')")
     public ResponseEntity<?> updateAnswer(
