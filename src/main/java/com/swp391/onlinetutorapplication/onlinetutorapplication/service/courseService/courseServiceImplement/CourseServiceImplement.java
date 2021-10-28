88274bde90cd4c26147f727a08d84e246d06a535
Huy <giahuy120701@gmail.com>
2021-10-28 15:58:44 +0700

    Fix get timetable list

--- src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/courseService/courseServiceImplement/CourseServiceImplement.java
+++ src/main/java/com/swp391/onlinetutorapplication/onlinetutorapplication/service/courseService/courseServiceImplement/CourseServiceImplement.java
@@ -527,7 +527,7 @@ public class CourseServiceImplement implements CourseServiceInterface {
         if (tutor.getExpireAuthorization().isBefore(Instant.now())) {
             userService.handleUserLogout(accessToken);
         }
-        Course course = courseRepository.findById(courseId)
+        Course course = courseRepository.findByIdAndStatusIsTrue(courseId)
                 .orElseThrow(() -> {
                     throw new NoSuchElementException("Course cannot be found");
                 });
@@ -546,7 +546,7 @@ public class CourseServiceImplement implements CourseServiceInterface {
 
     @Override
     public List<TimeTableInformation> getTimeTableList(Long courseId) throws Exception {
-        Course course = courseRepository.findById(courseId).
+        Course course = courseRepository.findByIdAndStatusIsTrue(courseId).
                 orElseThrow(() -> {
                     throw new NoSuchElementException("Course cannot be found");
                 });
